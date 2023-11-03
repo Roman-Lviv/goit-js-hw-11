b5ac2f8d7e1962f01a2ea83f6e2c5eb89a4d3eb9
Roman-Lviv <kituraroman@gmail.com>
2023-11-03 20:51:20 +0200

    git add .

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,5 @@
 export const searchImages = async (searchQuery, page) => {
-  const apiKey =
-    'live_NhSrRxtwZj7uypmgbjo4Tmh4DCoEpAzSYngICE3X5fge2sZZWwQja8SMmW7HlaJe';
+  const apiKey = '40461554-615dcd3ac736ed83d077c5b83';
   const perPage = 40;
 
   const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
