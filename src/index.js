6c4a3e1729de041f909714db3c678ec3c6f30d74
Roman-Lviv <kituraroman@gmail.com>
2023-11-05 20:19:38 +0200

    git add .

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ Notiflix.Notify.init();
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
+
 let page = 1;
 let query = '';
 
--- src/js/api.js
+++ src/js/api.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const apiKey = '40461554-615dcd3ac736ed83d077c5b83';
 const perPage = 40;
 
+const axios = require('axios');
+
 export const searchImages = async (searchQuery, page) => {
   const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
