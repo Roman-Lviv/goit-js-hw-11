9867d8a326a9d195208aec5171d1aa05b5bf3cec
Roman-Lviv <kituraroman@gmail.com>
2023-11-05 13:12:15 +0200

    git add .

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import 'notiflix';
 import Notiflix from 'notiflix';
 import { searchImages } from './js/api';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+Notiflix.Notify.init();
 
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
-const endMessage = document.querySelector('.end-message'); // Повідомлення про кінець результатів
 let page = 1;
 let query = '';
 
 searchForm.addEventListener('submit', async e => {
   e.preventDefault();
-  query = searchForm.searchQuery.value;
+  query = searchForm.searchQuery.value.trim(); 
   page = 1;
   clearGallery();
+  if (!query) return; 
   await performSearch(query, page);
 });
 
@@ -29,67 +32,75 @@ const clearGallery = () => {
 const displayImages = images => {
   const cardTemplate = image => `
     <div class="photo-card">
-      <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-        <p class="info-item"><b>Views:</b> ${image.views}</p>
-        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-      </div>
+        <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+            <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+            <p class="info-item"><b>Views:</b> ${image.views}</p>
+            <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+            <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+        </div>
     </div>
   `;
 
-  images.forEach(image => {
-    gallery.innerHTML += cardTemplate(image);
-  });
+  const cardMarkup = images.map(image => cardTemplate(image)).join(''); 
 
-  const lightbox = new SimpleLightbox('.gallery a', {});
+  gallery.innerHTML = cardMarkup; 
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+   
+  });
 
   if (images.length === 0) {
     loadMoreButton.style.display = 'none';
-    notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
+    Notiflix.Notify.failure(
+      'На жаль, не знайдено зображень, що відповідають вашому запиту. Спробуйте ще раз.'
     );
-    endMessage.style.display = 'none';
   } else {
     loadMoreButton.style.display = 'block';
   }
 };
 
-const performSearch = async (searchQuery, page) => {
-  if (!searchQuery.trim()) {
-    notiflix.Notify.warning('Please enter a search keyword.');
+const performSearch = (searchQuery, page) => {
+  if (!searchQuery) {
+    Notiflix.Notify.warning('Введіть ключове слово для пошуку.');
     return;
   }
 
-  const data = await searchImages(searchQuery, page);
+  searchImages(searchQuery, page)
+    .then(data => {
+      if (data) {
+        if (data.totalHits > 0) {
+          displayImages(data.hits);
 
-  if (data) {
-    if (data.totalHits > 0) {
-      notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      displayImages(data.hits);
+          if (page === 1) {
+            Notiflix.Notify.success(`Ура! Ми знайшли ${data.totalHits} зображень.`);
+          }
 
-      const { height: cardHeight } =
-        gallery.firstElementChild.getBoundingClientRect();
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
+          const { height: cardHeight } =
+            gallery.firstElementChild.getBoundingClientRect();
+          window.scrollBy({
+            top: cardHeight * 2,
+            behavior: 'smooth',
+          });
+        } else {
+          Notiflix.Notify.failure(
+            'На жаль, не знайдено зображень, що відповідають вашому запиту. Спробуйте ще раз.'
+          );
+        }
+
+        if (data.totalHits <= page * data.hits.length || data.totalHits < 40) {
+          loadMoreButton.style.display = 'none';
+          Notiflix.Notify.warning(
+            'На жаль, ви дійшли до кінця результатів пошуку.'
+          );
+        }
+      })
+      .catch(error => {
+        Notiflix.Notify.failure(
+          'Сталася помилка під час пошуку зображень. Спробуйте ще раз.'
+        );
+        console.error('Помилка під час пошуку зображень:', error);
       });
-    } else {
-      notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      loadMoreButton.style.display = 'none';
-      endMessage.style.display = 'none';
-    }
-
-    if (data.totalHits <= page * data.hits.length) {
-      loadMoreButton.style.display = 'none';
-      endMessage.style.display = 'block';
-    } else {
-      endMessage.style.display = 'none';
-    }
-  }
 };
--- src/js/api.js
+++ src/js/api.js
@@ -1,15 +1,16 @@
-export const searchImages = async (searchQuery, page) => {
-  const apiKey = '40461554-615dcd3ac736ed83d077c5b83';
-  const perPage = 40;
+import axios from 'axios';
 
+const apiKey = '40461554-615dcd3ac736ed83d077c5b83';
+const perPage = 40;
+
+export const searchImages = async (searchQuery, page) => {
   const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
   try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data;
+    const response = await axios.get(apiUrl);
+    return response.data;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Помилка при отриманні даних:', error);
     return null;
   }
 };
